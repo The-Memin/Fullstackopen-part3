29649b1b09cde929624aadf8f79228c50fd580c5
The-Memin <guillejuma@outlook.es>
2025-05-23 12:46:37 -0600

    Part 3 - Exercise 3.2: phonebookBackend, step 2

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (request, response) =>{
     response.json({persons})
 })
 
+app.get('/info', (request, response) =>{
+    const currentDate = new Date()
+
+    const html_response = `<div>
+                            <p>Phonebook has info for ${persons.length} people</p>
+                            <p>${currentDate}</p>
+                          </div>`
+
+    response.send(html_response)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
