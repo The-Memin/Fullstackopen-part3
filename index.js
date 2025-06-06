1a679788f83f90cef307111cc3c8c89ff9b0cd0b
The-Memin <guillejuma@outlook.es>
2025-06-06 15:10:17 -0600

    Part 3 - Exercise 3.14: Phonebook database, step 2

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
 const app = express()
+const Person = require('./models/person')
+
 
 app.use(express.json())
 
@@ -14,98 +16,54 @@ app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors())
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "phone": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "phone": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "phone": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "phone": "39-23-6423122"
-    }
-]
+let people = []
 
-app.get('/api/persons', (request, response) =>{
-    response.json(persons)
+app.get('/api/people', (request, response) =>{
+  Person.find({}).then(p => {
+    people = p
+    response.json(p)
+  })
 })
 
 app.get('/info', (request, response) =>{
     const currentDate = new Date()
 
     const html_response = `<div>
-                            <p>Phonebook has info for ${persons.length} people</p>
+                            <p>Phonebook has info for ${people.length} people</p>
                             <p>${currentDate}</p>
                           </div>`
 
     response.send(html_response)
 })
 
-app.get('/api/persons/:id', (request, response)=>{
-  const id = request.params.id
-  const person = persons.find(p => p.id.toString() === id)
-
-  person 
-    ? response.json(person) 
-    : response.status(404).end()
-  
+app.get('/api/people/:id', (request, response)=>{
+  Person.findById(request.params.id).then( person =>{
+    response.json(person)
+  })
 })
 
-app.delete('/api/persons/:id', (request, response)=>{
-  const id = request.params.id
-  persons = persons.filter(p => p.id.toString() !== id)
-
-  response.status(204).end()
+app.delete('/api/people/:id', (request, response)=>{
+  Person.deleteOne({_id: request.params.id}).then(_ =>{
+    response.json(_)
+  })
 })
 
 
-app.post('/api/persons/', (request, response)=>{
-  const {name, phone} = request.body
-  
-  if(!name || !phone){
-    return response.status(400).json({
-      error: 'Name and phone number are required'
-    })
+app.post('/api/people/', (request, response)=>{
+  const body = request.body
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
-  // Validar si el nombre ya está en la agenda
-  const nameExists = persons.some(person => person.name.toLowerCase() === name.toLowerCase());
-  if (nameExists) {
-    return response.status(409).json({ 
-      error: 'Name already exists in the phonebook' 
-    });
-  }
-
   // Crear y agregar nueva persona
-  const newPerson = {
-    id: generateID(),
-    name,
-    phone
-  };
+  const person = Person({
+    name: body.name,
+    phone: body.phone
+  });
 
-  persons = persons.concat(newPerson);
-  response.status(201).json(newPerson);
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
-
-const generateID = ()=> {
-  const caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let id = '';
-  for (let i = 0; i < 5; i++) {
-    const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
-    id += caracteres.charAt(indiceAleatorio);
-  }
-  return Date.now() + "_" + id;
-}
 
 
 const PORT = process.env.PORT || 3001
